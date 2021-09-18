190cf1d488ecd2d487b73c0033da3de4ac1acb61
YunJiHun <yjhzzing4699@naver.com>
2021-09-18 19:09:35 +0900

    도시 관련 data class 생성

---
+++ app/src/main/java/com/jihun/searchweather/data/CityInfo.kt
@@ -0,0 +1,20 @@
+package com.jihun.searchweather.data
+
+import com.google.gson.annotations.SerializedName
+import com.jihun.searchweather.ViewType
+
+data class MainModule(
+    var type: ViewType = ViewType.EMPTY,
+    var data: Any? = null,
+)
+
+data class CityHeader(
+    val name: String?,
+    val count: Int
+)
+
+data class CityInfo (
+    @SerializedName("id") val id: Int,
+    @SerializedName("country") val country: String?,
+    @SerializedName("name") val name: String?
+)
