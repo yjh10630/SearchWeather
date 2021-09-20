dc78fbc29d8b013916c918438d1831f6117f6c7d
YunJiHun <yjhzzing4699@naver.com>
2021-09-20 21:37:54 +0900

    open class -> data class

--- app/src/main/java/com/jihun/searchweather/data/RouterEvent.kt
+++ app/src/main/java/com/jihun/searchweather/data/RouterEvent.kt
@@ -3,7 +3,7 @@ package com.jihun.searchweather.data
 
 const val INTENT_EXTRA_STRING_PARAM = "intent_extra_string_param"
 
-open class RouterEvent (
+data class RouterEvent (
     val type: Landing,
     var paramString: String? = null
 )
